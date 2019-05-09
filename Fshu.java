deff49b35f4167f5fab2b64385d9687f5f27b501
Suecaptain <48464107+Suecaptain@users.noreply.github.com>
2019-05-09 20:38:51 +0800

    Create Fshu.java

---
+++ Fshu.java
@@ -0,0 +1,29 @@
+public class Fshu{
+   static int F(int a){
+    public static void main(String[] args) throws IOException
+	{
+		int a = 0, b=1,n,num;
+		String str;
+		BufferedReader buf;
+		buf = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print();
+		str =buf.readline();
+		num =Integer.parseInt(str);
+		while(b<num)
+		{
+			n = a+b;
+			a=b;
+			b=n;
+		}
+		
+		/* if(num==b) System.out,prinln(num+"是F数");
+		else System.out.println(num+"不是F数")； */
+	}
+	return b;
+   }
+public static void main(String args[]){
+    int z;
+	z = F();
+    if(z==F.) System.out,prinln(num+"是F数");
+		else System.out.println(num+"不是F数");`
+}
