2af5dd43d4053e16ba067223509be966dc5abe77
Suecaptain <48464107+Suecaptain@users.noreply.github.com>
2019-05-09 20:35:42 +0800

    Create App3_7.java

---
+++ App3_7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class App3_7
+{
+	public static void main(String[] args)
+	{
+		String s1 , s2 ;
+		Scanner reader = new Scanner(System.in);
+		System.out.print("请输入第一个数据：");
+		//s1 = reader.next();
+		//s2 = reader.next();
+		//System.out.print("请输入第二个数据：");
+		s2 = reader.nextLine();
+		System.out.print("输入的是"+s2);
+	}
+	
+}
